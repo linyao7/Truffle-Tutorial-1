fbbbf7c7d7a5c2d5dc8691f5444ba67cea94ed30
theMoonOnly <theMoonOnly7@gmail.com>
2021-10-13 13:26:11 +0800

    add all files

---
+++ contracts/Helloworld.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+contract Helloworld {
+    string public message;
+    address public owner;
+
+    constructor(string memory _message) {
+        message = _message;
+        owner = msg.sender;
+    }
+
+    function hello() public view returns (string memory) {
+        return message;
+    }
+
+    function setMessage(string memory _message) public payable {
+        require(msg.sender == owner);
+        message = _message;
+    }
+}
+
---
+++ contracts/Helloworld2.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+contract HelloworldPermanent {
+    string public message;
+    address public owner;
+
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    
+}
+
